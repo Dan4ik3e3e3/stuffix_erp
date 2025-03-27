4a55dbabe94827bc658a00c9b95700b1d256495b
Victoria <dkuzmitskiyy505@gmail.com>
2025-03-27 19:44:01 +0500

    Отключен ESLint при сборке и обновлен URL

--- next.config.js
+++ next.config.js
@@ -2,13 +2,17 @@
 const nextConfig = {
   output: 'standalone',
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://stuffix.online',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'https://stuffix-erp-y61c-git-main-danils-projects-dad042f5.vercel.app',
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
   images: {
     domains: ['stuffix.online', 'www.stuffix.online', 'ui-avatars.com'],
   },
   reactStrictMode: true,
+  eslint: {
+    // Отключаем проверку ESLint при сборке
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
