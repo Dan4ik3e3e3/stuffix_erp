a9d452db8011098add9c0092664aeacd8b312e08
Victoria <dkuzmitskiyy505@gmail.com>
2025-03-27 21:47:47 +0500

    Перенесена страница регистрации в директорию app

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Alert, Box, Button, Container, TextField, Typography } from '@mui/material';
 
 export default function RegisterPage() {
